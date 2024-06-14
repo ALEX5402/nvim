da8765a8cc153548134a4687a0eaeb425025dcbe
Alex <alex5402private@gmail.com>
2024-06-15 04:16:44 +0530

    cleanup

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,10 +7,10 @@ local M = {}
 M.ui = {
 	theme = "chadracula-evondev",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
 }
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,9 +2,33 @@ require "nvchad.mappings"
 
 -- add yours here
 
-local map = vim.keymap.set
+local M = {}
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+M.crates = {
+  plugin = true,
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require('crates').upgrade_all_crates()
+      end,
+      "update crates"
+    }
+  }
+}
+
+return M
