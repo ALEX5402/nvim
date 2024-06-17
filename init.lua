454958f5ab14a389bb5a1375f78d7341268e034b
Alex <alex5402private@gmail.com>
2024-06-17 17:36:36 +0530

    done On my configs

--- init.lua
+++ init.lua
@@ -1,81 +1,111 @@
-require "user.launch"
-require "user.options"
-require "user.keymaps"
-require "user.autocmds"
-spec "user.colorscheme"
-spec "user.devicons"
-spec "user.treesitter"
-spec "user.mason"
-spec "user.schemastore"
-spec "user.lspconfig"
-spec "user.navic"
-spec "user.breadcrumbs"
-spec "user.null-ls"
-spec "user.illuminate"
-spec "user.telescope"
-spec "user.telescope-tabs"
--- spec "user.nvimtree"
-spec "user.neotree"
-spec "user.lualine"
-spec "user.whichkey"
-spec "user.dap"
--- spec "user.autotag"
--- spec "user.sleuth"
-spec "user.cmp"
-spec "user.autopairs"
-spec "user.comment"
-spec "user.gitsigns"
-spec "user.neogit"
-spec "user.diffview"
-spec "user.indentline"
-spec "user.alpha"
-spec "user.netrw"
-spec "user.project"
-spec "user.toggleterm"
-spec "user.bufdelete"
-spec "user.luarocks"
-spec "user.image"
--- Extras
-spec "user.extras.colorizer"
--- spec "user.extras.smoothie"
-spec "user.extras.neoscroll"
--- spec "user.extras.copilot"
-spec "user.extras.modicator"
-spec "user.extras.rainbow"
-spec "user.extras.matchup"
-spec "user.extras.bqf"
-spec "user.extras.dial"
-spec "user.extras.nui"
-spec "user.extras.ufo"
-spec "user.extras.dressing"
-spec "user.extras.surround"
-spec "user.extras.eyeliner"
-spec "user.extras.numb"
-spec "user.extras.jaq"
-spec "user.extras.navbuddy"
--- spec "user.extras.minifiles"
-spec "user.extras.oil"
--- spec "user.extras.noice"
-spec "user.extras.fidget"
-spec "user.extras.neotab"
-spec "user.extras.neotest"
-spec "user.extras.cmp-tabnine"
-spec "user.extras.lab"
-spec "user.extras.tabby"
-spec "user.extras.various-textobjs"
-spec "user.extras.spider"
--- spec "user.extras.test"
-spec "user.extras.nostr"
-spec "user.extras.typescript-tools"
-spec "user.extras.tsc"
-spec "user.extras.gitlinker"
-spec "user.extras.fugitive"
-spec "user.extras.harpoon"
--- spec "user.extras.trailblazer"
-spec "user.extras.package-info"
-spec "user.extras.todo-comments"
-spec "user.extras.trouble"
-spec "user.extras.cellular-automaton"
-spec "user.extras.rustacean"
-spec "user.extras.crates"
-require "user.lazy"
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+local lazy_config = {}
+
+local plugins = {
+  -- plugins are here 
+    {  
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+   },
+      {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        config = function()
+          require "configs.conform"
+        end,
+      },
+     {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      config = {
+        center = {
+          {
+            icon = '',
+            icon_hl = 'group',
+            desc = 'description',
+            desc_hl = 'group',
+            key = 'shortcut key in dashboard buffer not keymap !!',
+            key_hl = 'group',
+            key_format = ' [%s]', -- `%s` will be substituted with value of `key`
+            action = '',
+          },
+        },
+        footer = {},
+      }
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
+
+  
+      -- These are some examples, uncomment them if you want to see them work!
+      -- {
+      --   "neovim/nvim-lspconfig",
+      --   config = function()
+      --     require("nvchad.configs.lspconfig").defaults()
+      --     require "configs.lspconfig"
+      --   end,
+      -- },
+      --
+      -- {
+      -- 	"williamboman/mason.nvim",
+      -- 	opts = {
+      -- 		ensure_installed = {
+      -- 			"lua-language-server", "stylua",
+      -- 			"html-lsp", "css-lsp" , "prettier"
+      -- 		},
+      -- 	},
+      -- },
+      --
+      -- {
+      -- 	"nvim-treesitter/nvim-treesitter",
+      -- 	opts = {
+      -- 		ensure_installed = {
+      -- 			"vim", "lua", "vimdoc",
+      --      "html", "css"
+      -- 		},
+      -- 	},
+      -- },
+}
+
+require("lazy").setup({
+  plugins,
+  lazy_config
+})
+
+require("configs.lspconfig")
+require("configs.conform")
+
+
+
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults") 
+dofile(vim.g.base46_cache .. "statusline")
+
+require "nvchad.autocmds"
+
+
+
+vim.schedule(function()
+  require "mappings"
+end)
---
+++ lua/chadrc.lua
@@ -0,0 +1,12 @@
+---@type ChadrcConfig
+local M = {}
+
+
+local options = {}
+
+
+M.ui = {
+	theme = "chadracula-evondev"
+}
+
+return M
